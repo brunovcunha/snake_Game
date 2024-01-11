b45fbe1be670926d0a947f50801d7005197868eb
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-01-10 21:57:14 -0300

    modified:   css/style.css #	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,40 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+
+const size = 30;
+
+const snake = [
+    {x: 200, y: 200},
+    {x: 230, y: 200},
+];
+
+let direction = "right"
+
+const drawSnake = () => {
+    ctx.fillStyle = "#ddd"
+
+    snake.forEach((position, index) => {
+        if(index == snake.length - 1){
+            ctx.fillStyle = "white"
+        }
+        ctx.fillRect(position.x, position.y, size, size)
+    })
+}
+
+const moveSnake = () =>{
+    const head = snake[snake.length - 1]
+
+    snake.shift();
+
+    if(direction == "right"){
+        snake.push({x: head.x + 30, y: head.y})
+    }
+}
+
+setInterval(() => {
+    ctx.clearRect(0, 0, 600, 600)
+    moveSnake()
+    drawSnake();
+},300)
+
